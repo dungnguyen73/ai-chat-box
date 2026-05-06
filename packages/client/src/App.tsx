8529352de1d051b9835f837c00ba4a14a9475102
Nguyễn Tấn Dũng <nguyentandung037@gmail.com>
2026-05-06 17:18:41 +0700

    feat: add App component for health checks and update project formatting configuration

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,52 +1,60 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import './App.css'
+import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [isShowHealth, setIsShowHealth] = useState(false);
+    useEffect(() => {
+        fetch('/api/health')
+            .then((res) => res.json())
+            .then((data) => {
+                setMessage(JSON.stringify(data));
+            })
+            .catch((err) => {
+                console.log(err);
+                setMessage(JSON.stringify(err));
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
 
-  const [message, setMessage] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [isShowHealth, setIsShowHealth] = useState(false);
-  useEffect(() => {
-    fetch('/api/health')
-      .then(res => res.json())
-      .then(data => {
-        setMessage(JSON.stringify(data));
-      })
-      .catch(err => {
-        console.log(err);
-        setMessage(JSON.stringify(err));
-      })
-      .finally(() => {
-        setLoading(false);
-      })
-  }, [])
-
-  const getHealthCheckStatus = () => {
-    setLoading(true);
-    fetch('/api/health')
-      .then(res => res.json())
-      .then(data => {
-        setMessage(JSON.stringify(data));
-      })
-      .catch(err => {
-        console.log(err);
-        setMessage(JSON.stringify(err));
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-  return (
-    <>
-      <Button variant="outline" onClick={() => {
-        getHealthCheckStatus();
-                  setIsShowHealth(!isShowHealth)
-                }}>{isShowHealth ? `Hide Health` : `Click to view Health`}</Button>
-      {isShowHealth && <p className='text-center text-2xl font-bold mt-4'>health check status: {loading ? "Loading..." : message}</p>}
-    </>
-  )
+    const getHealthCheckStatus = () => {
+        setLoading(true);
+        fetch('/api/health')
+            .then((res) => res.json())
+            .then((data) => {
+                setMessage(JSON.stringify(data));
+            })
+            .catch((err) => {
+                console.log(err);
+                setMessage(JSON.stringify(err));
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+    return (
+        <>
+            <Button
+                variant="outline"
+                onClick={() => {
+                    getHealthCheckStatus();
+                    setIsShowHealth(!isShowHealth);
+                }}
+            >
+                {isShowHealth ? `Hide Health` : `Click to view Health`}
+            </Button>
+            {isShowHealth && (
+                <p className="text-center text-2xl font-bold mt-4">
+                    health check status: {loading ? 'Loading...' : message}
+                </p>
+            )}
+        </>
+    );
 }
 
-export default App
+export default App;
