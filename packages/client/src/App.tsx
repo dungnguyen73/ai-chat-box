f67978f8e7f9292fd10bf4dc76332a5628ed4134
Nguyễn Tấn Dũng <nguyentandung037@gmail.com>
2026-05-06 17:01:57 +0700

    feat: setup Shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
 
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(true);
+  const [isShowHealth, setIsShowHealth] = useState(false);
   useEffect(() => {
     fetch('/api/health')
       .then(res => res.json())
@@ -20,14 +22,30 @@ function App() {
         setLoading(false);
       })
   }, [])
+
+  const getHealthCheckStatus = () => {
+    setLoading(true);
+    fetch('/api/health')
+      .then(res => res.json())
+      .then(data => {
+        setMessage(JSON.stringify(data));
+      })
+      .catch(err => {
+        console.log(err);
+        setMessage(JSON.stringify(err));
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  }
   return (
-    <main className='min-h-screen bg-gradient-to-b from-white via-orange-50 to-yellow-100 dark:from-gray-900 dark:via-slate-800 dark:to-slate-950 transition-colors duration-300 font-sans antialiased'>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <p className='text-2xl font-bold text-center text-gray-800 dark:text-gray-200'>health check status: {message}</p>
-      )}
-    </main>
+    <>
+      <Button variant="outline" onClick={() => {
+        getHealthCheckStatus();
+        setIsShowHealth(!isShowHealth)
+      }}>{isShowHealth ? `Hide Health` : `Click to view Health`}</Button>
+      {isShowHealth && <p className='text-center text-2xl font-bold mt-4'>health check status: {loading ? "Loading..." : message}</p>}
+    </>
   )
 }
 
